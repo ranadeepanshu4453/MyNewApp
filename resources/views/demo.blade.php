da848128166a7df890f71b81457515d261f32128
MyNewApp <ranadeepanshu985@gmail.com>
2024-08-14 12:56:38 +0530

    external css-js added

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -1,5 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/firstcss.css" />
+    <script src="js/firstscript.js"></script>
+</head>
+<body>
 <div>
     <h2>This is demo page</h2>
+    <button onclick="clicked()">click</button>
 
-   
 </div>
+</body>
+</html>
